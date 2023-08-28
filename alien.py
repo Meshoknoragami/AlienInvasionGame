d7486d4699b050b002645a20421f10b127b41b8d
Meshoknoragami <138443070+Meshoknoragami@users.noreply.github.com>
2023-08-28 12:53:08 +0500

    Update alien.py

--- alien.py
+++ alien.py
@@ -10,11 +10,11 @@ class Alien(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
-        # загрузка изображения пришельца и назначение атрибута rect
+        # загрузка изображения пришельца и назначение атрибута rect.
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
-        # каждый новый пришелец появляется в левом верхнем углу экрана
+        # каждый новый пришелец появляется в левом верхнем углу экрана.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
